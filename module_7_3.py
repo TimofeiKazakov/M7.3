255e430b493e96b6f46be54a85b8dec9a2e45012
TimofeiKazakov <violet09502@gmail.com>
2024-11-28 18:58:58 +0300

    Add files via upload

---
+++ module_7_3.py
@@ -0,0 +1,34 @@
+class WordsFinder:
+    def __init__(self, *file_names):
+        self.file_names = file_names
+
+    def get_all_words(self):
+        all_words = {}
+        for i in self.file_names:
+            with open(i, 'r', encoding='utf-8') as file:
+                file = file.read().lower()
+                for j in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    file = file.replace(j, '')
+                words = file.split()
+                all_words[i] = words
+        return all_words
+
+    def find(self, word):
+        find = {}
+        for self.file_num in self.file_names:
+            for key, value in self.get_all_words().items():
+                word = word.upper()
+                if word in [x.upper() for x in value]:
+                    find[key] = (value.index(word.lower()) + 1)
+            return find
+
+    def count(self, word):
+        count = {}
+        for i, word_ in self.get_all_words().items():
+            count[i] = word_.count(word.lower())
+        return count
+
+finder2 = WordsFinder('test_file.txt')
+print(finder2.get_all_words()) # Все слова
+print(finder2.find('TEXT')) # 3 слово по счёту
+print(finder2.count('teXT')) # 4 слова teXT в тексте всего
